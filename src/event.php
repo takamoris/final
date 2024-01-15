d6ce2a91a38c136cc9ee4c6600b42574e1861c3f
takamoris <2201367@s.asojuku.ac.jp>
2024-01-15 12:34:40 +0900

    upload

--- src/event.php
+++ src/event.php
@@ -1,2 +1,33 @@
 <?php session_start(); ?>
 <?php require 'db-connect.php'; ?>
+<form action="event.php">
+    イベント検索
+    <input type="text" name="keyword">
+    <input type="submit" value="検索">
+</form>
+<hr>
+<?php
+    echo '<table>';
+    echo '<tr><th>イベント番号</th><tr>イベント名</tr><tr>説明</tr><tr>場所名</tr><tr>住所</tr><tr>開催日時</tr><tr>終了日時</tr><tr>画像</tr></tr>';
+    $pdo = new PDO ($connect,USER,PASS);
+    if(isset($_POST['keyword'])){
+        $sql = $pdo -> prepare('select * from Event where event_name like ?');
+        $sql -> execute(['%'.$_POST['keyword'].'%']);
+    }else{
+        $sql = $pdo -> query('select * from Event');
+    }
+    foreach($sql as $row){
+        $id = $row ['event_id'];
+        echo '<tr>';
+        echo '<td>',$id,'</td>';
+        echo '<td>',$row['event_name'],'</td>';
+        echo '<td>',$row['explanation'],'</td>';
+        echo '<td>',$row['place_name'],'</td>';
+        echo '<td>',$row['address'],'</td>';
+        echo '<td>',$row['start_day'],'</td>';
+        echo '<td>',$row['finish_day'],'</td>';
+        echo '<td>',$row['img_id'],'</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+    ?>
--- src/top-page.php
+++ src/top-page.php
@@ -7,6 +7,7 @@
 </head>
 <body>
 <h1>メニュー</h1>
+<hr>
 <a href="event.php"><button type="submit">イベント一覧</button></a>
 <a href="event-add.php"><button type="submit">新規イベント登録</button></a>
 <a href="event-update.php"><button type="submit">イベント更新</button></a>
