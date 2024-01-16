a7ad59fbe69deac5f2f9eea01059685a6d6f4be0
takamoris <2201367@s.asojuku.ac.jp>
2024-01-16 13:40:31 +0900

    upload

--- src/event-add-input.php
+++ src/event-add-input.php
@@ -1,7 +1,7 @@
 <?php session_start(); ?>
 <?php require 'db-connect.php'; ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
