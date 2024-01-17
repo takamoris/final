51362c4526be06f298d4ebf8671fb753dda531e1
takamoris <2201367@s.asojuku.ac.jp>
2024-01-17 16:16:35 +0900

    upload

--- src/top-page.php
+++ src/top-page.php
@@ -10,7 +10,6 @@
 <hr>
 <a href="event.php"><button type="submit">イベント一覧</button></a>
 <a href="event-add-input.php"><button type="submit">新規イベント登録</button></a>
-<a href="event-update.php"><button type="submit">イベント更新</button></a>
-<a href="event-delete.php"><button type="submit">イベント削除</button></a>    
+<a href="event-update.php"><button type="submit">イベント更新</button></a>    
 </body>
 </html>
